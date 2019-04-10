4d20c4ba8e055342eb4ee659d78d8fcbea8b94ba
Erkin <erkash88@gmail.com>
2019-04-10 14:43:44 +0600

    Youtube api iframe integrated

--- src/js/script.js
+++ src/js/script.js
@@ -97,6 +97,81 @@ more.addEventListener('click', () => {
         videosWrapper.appendChild(card);
         setTimeout(() => {
             card.classList.remove('videos__item-active')
-        }, 10)
+        }, 10);
+        bindNewModal(card);
+    }
+
+    sliceTitle('.videos__item-descr', 100);
+});
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+        
+        if (item.textContent.length < count) {
+            return;
+        }
+        item.textContent = item.textContent.slice(0, count + 1) + "...";
+    })
+}
+
+sliceTitle('.videos__item-descr', 100);
+
+function openModal() {
+    modal.style.display = 'block'
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModal(cards) {
+    cards.forEach(card => {
+        card.addEventListener('click', (e) => {
+            e.preventDefault();
+            let id = card.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        })
+    })
+}
+
+function bindNewModal(cards) {
+    cards.addEventListener('click', (e) => {
+        e.preventDefault();
+        let id = cards.getAttribute('data-url');
+        loadVideo(id);
+        openModal();
+    })
+}
+
+bindModal(videos);
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
     }
 });
+
+function createVideo() {
+    let tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE'
+        });
+    }, 300)
+}
+
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
